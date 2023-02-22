8938a4ac4e8f84998d81ce2c6c37fbc5e27836b1
muhammadadnanoukal <101559768+muhammadadnanoukal@users.noreply.github.com>
2023-02-22 09:37:26 +0300

    Add files via upload

---
+++ ALTANMYA_Project_Button/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'ALTANMYA PROJECT BUTTON',
+    'version': '1.0',
+    'summary': 'ALTANMYA PROJECT BUTTON',
+    'sequence': 10,
+    'description': """ ALTANMYA PROJECT BUTTON """,
+    'category': 'Productivity',
+    'website': 'https://www.odoomates.tech',
+    'depends': ['project', 'purchase'],
+    'data': [
+        'views/purchase_button_view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+}
---
+++ ALTANMYA_Project_Button/model/__init__.py
@@ -0,0 +1,1 @@
+from . import  purhcase_button
